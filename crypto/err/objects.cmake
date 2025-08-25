22b10e91f9c8cc16deb1f9f62558dfe262ad55f6
Dan Rosser <danoli3@gmail.com>
2025-08-25 16:06:48 +1000

    cmake updates for 3.5

--- crypto/err/objects.cmake
+++ crypto/err/objects.cmake
@@ -10,3 +10,14 @@ set(LIBCRYPTO_CURRENTDIR_SOURCES
     ${CMAKE_SOURCE_DIR}/openssl/crypto/err/err_mark.c
     ${CMAKE_SOURCE_DIR}/openssl/crypto/err/err_save.c
 )
+
+
+if(EXISTS "${CMAKE_SOURCE_DIR}/openssl/crypto/err/ssl_err.c")
+    list(APPEND LIBCRYPTO_CURRENTDIR_SOURCES
+         ${CMAKE_SOURCE_DIR}/openssl/crypto/err/ssl_err.c)
+endif()
+
+if(EXISTS "${CMAKE_SOURCE_DIR}/openssl/ssl/ssl_err_legacy.c")
+    list(APPEND LIBSSL_CURRENTDIR_SOURCES
+         ${CMAKE_SOURCE_DIR}/openssl/ssl/ssl_err_legacy.c)
+endif()
